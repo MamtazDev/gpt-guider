353d380b553a410247fae2422e2197cbd597ca89
mohi14 <mohiuddinkhan1409@gmail.com>
2023-10-03 17:51:09 +0600

    banner section done

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Banner from "@/components/Home/Banner/Banner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <h1>This is home</h1>
+      <Banner />
     </>
   );
 }
