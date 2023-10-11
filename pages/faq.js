7a6832c434ada1b1b6b8534fd0c482ed2f6e927e
mohi14 <mohiuddinkhan1409@gmail.com>
2023-10-11 18:12:11 +0600

    faq page done

---
+++ pages/faq.js
@@ -0,0 +1,171 @@
+import React from "react";
+
+const FaqPage = () => {
+  return (
+    <div className="container my-10">
+      <h4>Description</h4>
+      <h2>
+        About Us: We are a team of developers, academics, and investors trying
+        to solve problems in education through the integration of AI. Our
+        educational solutions are accurate, reliable, and rich, and our prices
+        are lower than any other platform. Our website is accessible worldwide&
+        supports all languages.
+      </h2>
+      <h4>
+        For business inquiries or to arrange an appointment with the founder,
+        please contact support with your inquiry."
+      </h4>
+      <h3>contact Information:</h3>
+      <p>#</p>
+      <div>
+        <p>For Egypt:</p>
+        <p>23 July Street, El-Bagour, Menofuia, Egypt.</p>
+        <p>Phone: +201226911067.</p>
+        <p>
+          Email: <span>support@gptguider.com.</span>
+        </p>
+        <p>
+          Gmail: <span> gptguider@gmail.com</span>
+        </p>
+      </div>
+      <h3>FAQ:</h3>
+      <p>#</p>
+      <p>What are GPT Guider services?</p>
+      <p>
+        GPT Guider provides educational AI services that focus on helping
+        students study more efficiently and productively.
+      </p>
+      <h3>What tools does GPT Guider provide?</h3>‌
+      <p>1. GPT Prof (including explanation & discussion)</p>
+      <p>2. Browsing plugin</p>
+      <p>3. Diagram plugin (including 10 types of diagrams & mind maps)</p>
+      <p>4. Chat with PDF (including uploading multiple PDF files & OCR)</p>
+      <p>5. AI Summaries & Notes</p>
+      <p>6. Q & A generator</p>
+      <p>7. Document OCR AI</p>
+      <p>8. Audio transcription & summarization</p>
+      <p>9. Video transcription & summarization</p>
+      <p>10. YouTube video transcription & summarization</p>
+      <h3>What AI models or technology does GPT Guider use?</h3>
+      <p>
+        We use a combination of LLMs like GPT-4, GPT-3.5, Whisper (OpenAI
+        voice-to-text model), AWS Polly neural voices, and Google Vision AI.
+      </p>
+      <h3>Who is the end user of GPT Guider?</h3>
+      <p>
+        GPT Guider can be used in all fields according to the tasks you want to
+        perform, but our primary end user is students, although it's also used
+        by academics, teachers, researchers, and more.
+      </p>
+      <h3>
+        How can I use the features available according to my subscription plan
+        or are there any guidelines for using the features?
+      </h3>
+      <p>
+        You will find FAQ pages for each feature that provide comprehensive
+        guidelines on how to use them and how they can benefit you.
+      </p>
+      <h3>
+        What is the difference between GPT Guider and other platforms that
+        provide educational AI services?
+      </h3>
+      <p>#</p>
+      <p>
+        GPT Guider stands out due to its advanced LLMs models like GPT-4 and
+        GPT-3.5. Our features are rich and can serve a wide range of tasks. We
+        offer competitive pricing and provide various local and global payment
+        methods. Our platform is user-friendly, with FAQ pages to guide you in
+        using the features effectively.
+      </p>
+      <h3>What languages does GPT Guider support?</h3>
+      <p>
+        GPT Guider supports all languages, including English, Chinese, Arabic,
+        German, French, Spanish, and more.
+      </p>
+      ‌<h3>What is the cancellation policy?</h3>
+      <p>#</p>
+      <p>
+        You can cancel your subscription plan at any time. If you cancel, you
+        will still have access to your plan until the end of the current 30-day
+        billing period. To cancel your subscription, send an email request
+        through any of the contact methods we provide.
+      </p>
+      <h3>What files are acceptable in GPT Guider?</h3>
+      <p>
+        GPT Guider currently accepts text-based PDF files. Scanned or
+        handwritten PDFs will be supported in the next update.
+      </p>
+      <h3>Is GPT Guider free?</h3>
+      <p>
+        No, GPT Guider is not free, but there is a free trial plan that is valid
+        for four days. The free trial includes:
+      </p>
+      <p>Free Trial: Try our features for 3 days, which includes:</p>
+      <p>1. Browsing plugin (20 messages)</p>
+      <p>2. Diagram plugin (20 diagrams & mind maps)</p>
+      <p>3. Chat with PDF, including OCR and uploading multiple PDF files</p>
+      <p>4. AI Summaries & Notes (unlimited)</p>
+      <p>5. Question & Answer generator (25 Q&A)</p>
+      <p>
+        For further information about subscription plans, please see the pricing
+        page.
+      </p>
+      <h3>What is GPT Prof?</h3>
+      <p>#</p>
+      <p>
+        GPT Prof is our most comprehensive AI tool, relying on the GPT-4 model.
+        It provides detailed explanations for your study materials and allows
+        for in-depth discussion. It offers explanations in both text and voice,
+        based on your language preference.
+      </p>
+      <h3>What is the browsing plugin?</h3>
+      <p>
+        The browsing plugin enables users to browse search engine results within
+        the chat interface. It provides clickable links, descriptive titles, and
+        concise summaries for each search result, enhancing your knowledge base
+        and access to up-to-date information.
+      </p>
+      <h3>How can I use the browsing plugin?</h3>
+      <p>
+        You can use the browsing plugin by either searching with a query or
+        providing a URL. It helps you summarize and understand long content
+        websites, providing immediate and updated knowledge.
+      </p>
+      <h3>What is the diagram plugin?</h3>
+      <p>
+        The diagram plugin enables you to create and edit diagrams and build
+        mind maps based on your data. You can upload your document, select the
+        pages you want to use, choose the type of diagram, or copy and paste the
+        data.
+      </p>
+      <h3>What types of diagrams can you build?</h3>
+      <p>
+        You can create various types of diagrams, including class diagrams,
+        state diagrams, sequence diagrams, flow charts, mind maps, pie charts,
+        ER charts, sequence diagrams, and quadrant charts.
+      </p>
+      <h3>How can the diagram plugin help you in your study?</h3>
+      <p>
+        The diagram plugin enhances understanding, facilitates efficient study,
+        and improves memory retention by providing visual representations of
+        complex topics and enabling customization of mind maps.
+      </p>
+      ‌<h3>Help:</h3>
+      <p>
+        If you encounter registration or payment issues, please don't hesitate
+        to contact us for assistance. You can reach us through the following
+        methods:
+      </p>
+      <p>For Egypt:</p>
+      <p>Phone: +201226911067.</p>
+      <p>
+        Email: <span>support@gptguider.com.</span>
+      </p>
+      <p>
+        Gmail: <span>gptguider@gmail.com.</span>
+      </p>
+    </div>
+  );
+};
+
+export default FaqPage;
